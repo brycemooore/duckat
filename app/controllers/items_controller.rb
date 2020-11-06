0a68d2ccfeb44081886e0ca76680ed589e933e8f
jrshort89 <jrshort89@gmail.com>
2020-11-06 14:25:09 -0500

    items skeleton done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,23 @@
 class ItemsController < ApplicationController
-    def show
-
+    def index
+        @items = Item.all
     end
 
-    def index
-
+    def show
+        @item = Item.find(params[:id])
     end
 
     def new
+        @item = Item.new
     end
 
     def create
+        @item = Item.new(item_params)
+        if @item.save
+            redirect_to item_path(@item)
+        else
+            render :new
+        end
     end
 
     def edit
@@ -20,5 +27,17 @@ class ItemsController < ApplicationController
     end
 
     def destroy
+    end
+
+    private 
+
+    def item_params
+        params.require(:item).permit(
+            :name, 
+            :description,
+            :seller_id,
+            :asking_price,
+            :end_date
+        )
     end
 end
